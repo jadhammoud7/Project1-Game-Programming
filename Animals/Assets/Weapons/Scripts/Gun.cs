1aef3aa175a97844201467f049fa1aeec9e935a7
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-03-09 00:12:56 +0200

    added weapon scripts for shooting

---
+++ Animals/Assets/Weapons/Scripts/Gun.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null){
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
