859678f14b974fc7f06b0b969fa941e18b96d668
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-03-12 11:50:05 +0200

    edited the light in the morning and evening

--- Animals/Assets/Hunter_house/House_scripts/hunter_house.cs
+++ Animals/Assets/Hunter_house/House_scripts/hunter_house.cs
@@ -6,14 +6,17 @@ public class hunter_house : MonoBehaviour
 {
     //[SerializeField] Light turn_off_lights;
     // Start is called before the first frame update
-    [SerializeField] Light mylight;
-    float constant_light_intensity;
+     [SerializeField] 
+    Light mylight;
+
     float light_intensity;
+    float first_intensity=70000;
     void Start()
     {
+        //mylight=GetComponent<Light>();
+        mylight.intensity=400;
         light_intensity=mylight.intensity;//to check the intensity at start
-        constant_light_intensity=mylight.intensity;//to save the intensity at start so that when hunters get awake the 
-        //sun will shine
+
     }
 
     // Update is called once per frame
@@ -24,25 +27,25 @@ public class hunter_house : MonoBehaviour
     public void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag=="Player"){
             Debug.Log("Hunter entered his house");
-            //other.gameObject.SetActive(false);
+
             Debug.Log("i am sleeping");
-            //turn_off_lights.enabled=false;
-            StartCoroutine(evening());
+
+             Debug.Log("light intensity is when i am sleeping: "+mylight.intensity);
+            StartCoroutine(evening());//starting the function everning by calling it
             }
         }
 public void OnTriggerExit(Collider other) {
-        //other.gameObject.SetActive(true);
         Debug.Log("i am awake");
-        //turn_off_lights.enabled=true;
-        StopCoroutine(evening());
-          mylight.intensity=  constant_light_intensity;
+        mylight.intensity=first_intensity;
+        StopAllCoroutines();
+
 
 }
  IEnumerator evening(){
-        for(float i=light_intensity;light_intensity>=0;light_intensity-=1f){
-            mylight.intensity=light_intensity;;
+    for(float i=light_intensity;light_intensity>=0;light_intensity-=1f){
+        mylight.intensity=light_intensity;
     }
-    yield return null;
+    yield return new WaitForSeconds(1f);//every iteration will take 1 sec
 } 
     }
 
