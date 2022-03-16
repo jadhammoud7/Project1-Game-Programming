f4f5e334227b643fdbc4eac8abefef1c2b2fb1cd
jadhammoud007 <91679985+jadhammoud007@users.noreply.github.com>
2022-03-16 13:49:14 +0200

    editing the health bar

--- Animals/Assets/all_characters/Hunter/hunter_scripts/Target.cs
+++ Animals/Assets/all_characters/Hunter/hunter_scripts/Target.cs
@@ -6,29 +6,26 @@ using UnityEngine.UI;
 public class Target : MonoBehaviour
 {
     [Tooltip("THe max health of this animal is 50.")]
-    public float health = 50f;
-    public Gradient gradient;
-    public Image fill;
-     
-    
-    public Slider slider;
-    // public void health_color(){
-    //     fill.color=gradient.Evaluate(1f);
-    // }
+    public float Maxhealth = 50f;
+    public float currentHealth=0f;
+    public health_bar health_bar1;
+    void Start()
+    {
+        currentHealth = Maxhealth;
+        health_bar1.setMaxHealth(Maxhealth);
+    }
 
-    public void TakeDamage(float amount){
-        health -= amount;
-        slider.value=health;
-        fill.color=gradient.Evaluate(slider.normalizedValue);
-        //health_color();
-        Debug.Log("health of "+ gameObject.name +" is now: " + health);
-        if(health <= 0f){
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        health_bar1.setHealth(currentHealth);
+        Debug.Log("health of " + gameObject.name + " is now: " + currentHealth);
+        if (currentHealth <= 0f)
+        {
             Destroy(gameObject);
         }
     }
-    //     void Start()
-    // {
-    //     health_color();
-    // }
+
+
 
 }
---
+++ Animals/Assets/all_characters/health_bar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class health_bar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public void setMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+    }
+    public void setHealth(float health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+        Debug.Log(fill.color);
+    }
+    void Start()
+    {
+        Debug.Log(fill.color);
+    }
+
+}
